931d649b26f8426cdd691445c8b5d9b6ae86da80
Maxime Barbier <maxime78280@hotmail.fr>
2019-01-18 12:29:27 +0100

    adminCategorieController

--- TP_GROUPE_17_01_2019/src/com/intiformation/gestion/commerce/controller/AdminCategoriesController.java
+++ TP_GROUPE_17_01_2019/src/com/intiformation/gestion/commerce/controller/AdminCategoriesController.java
@@ -19,65 +19,62 @@ import com.intiformation.gestion.commerce.metier.IAdminCategoriesMetier;
 @Controller
 @RequestMapping("/categorie")
 public class AdminCategoriesController {
-	
+
 	@Autowired
 	private IAdminCategoriesMetier adminCategorie;
-			
-	// ctor chargé pour injection de spring				
+
+	// ctor chargé pour injection de spring
 	public AdminCategoriesController(IAdminCategoriesMetier adminCategorie) {
 		super();
 		this.adminCategorie = adminCategorie;
 	}
 
-
-
-    /**
-     * Méthode index pour afficher la liste des produits et des catégories
-     * @return
-     */
-	@RequestMapping(value="/listCategories", method=RequestMethod.GET)
+	/**
+	 * Méthode index pour afficher la liste des produits et des catégories
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/listCategories", method = RequestMethod.GET)
 	public String index(Model model) {
 
-	// Création de la liste des categories 
-	List<Categorie> listeCategories = Collections.emptyList();		
-	listeCategories = adminCategorie.getListCategories();  
-	model.addAttribute("categoriesAttribute", listeCategories);
+		// Création de la liste des categories
+		List<Categorie> listeCategories = Collections.emptyList();
+		listeCategories = adminCategorie.getListCategories();
+		model.addAttribute("categoriesAttribute", listeCategories);
 
-	// nom de la vue : index | résolution : WEB-INF/views/categories.jsp
-	return "categories";
-	
-		}
-	
+		// nom de la vue : index | résolution : WEB-INF/views/categories.jsp
+		return "categories";
+
+	}
+
 	/**
 	 * Méthode delete catégorie
+	 * 
 	 * @param idcat
 	 * @param model
 	 */
-	@RequestMapping(value="/deleteCategorie", method=RequestMethod.GET)
-	//@RequestMapping(value="/deleteCategorie/{idcat}", method=RequestMethod.GET)
+	@RequestMapping(value = "/deleteCategorie", method = RequestMethod.GET)
+	// @RequestMapping(value="/deleteCategorie/{idcat}", method=RequestMethod.GET)
 	// en argument de suppcat => (@PathVariable long idcat)
-	public void suppCat(@RequestParam(required=true, value="idcat") Long idcat, Model model)  {
-		
-		//Categorie categorie = iAdminCategoriesMetier.getCategorie(idcat); Pas sûre d'en avoir besoin
-		adminCategorie.deleteCategorie(idcat);    
+	public void suppCat(@RequestParam(required = true, value = "idcat") Long idcat, Model model) {
+
+		// Categorie categorie = iAdminCategoriesMetier.getCategorie(idcat); Pas sûre
+		// d'en avoir besoin
+		adminCategorie.deleteCategorie(idcat);
 		model.addAttribute("categoriesAttribute", adminCategorie.getListCategories());
-		
-		//return "redirect:/index";
-	}	
-	
-	@RequestMapping(value="/save", method=RequestMethod.POST)
-	public String saveCat(@ModelAttribute("cat") Categorie cat ) {
-		
-		adminCategorie.addCategorie(cat);
-		
-		return "redirect:/listCategories";
-		
+
+		// return "redirect:/index";
 	}
-	
-	
-	
-	
-	
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveCat(@ModelAttribute("cat") Categorie cat) {
+
+		adminCategorie.addCategorie(cat);
+
+		return "redirect:/listCategories";
+
+	}
+
 	/**
 	 * afficher le formulaire en réponse à une requête GET pour la modification
 	 * 
@@ -85,16 +82,13 @@ public class AdminCategoriesController {
 	 */
 	@RequestMapping(value = "/updatecategorieform", method = RequestMethod.GET)
 	public ModelAndView setUpFormulaireUpdate(@RequestParam("catid") long pIdcategorie) {
-		
-		//1. recup du fonctionnaire de la bdd (c'est l'objet de commande)
-		Categorie categorie = iAdminCategoriesMetier.getCategorie(pIdcategorie);
-		
-		return new ModelAndView("editCategorie","categorieUpCommand", categorie);
+
+		// 1. recup du fonctionnaire de la bdd (c'est l'objet de commande)
+		Categorie categorie = adminCategorie.getCategorie(pIdcategorie);
+
+		return new ModelAndView("editCategorie", "categorieUpCommand", categorie);
 	}
-	
-	
-	
-	
+
 	/**
 	 * modifier une cat
 	 * @param model
@@ -104,63 +98,52 @@ public class AdminCategoriesController {
 	public String updateCategorie( @ModelAttribute("categorieUpCommand") Categorie pcategorie,
 			                           Model model) {
 		
-<<<<<<< HEAD
+
 		// Recup de la categorie par l'id
-		adminCategorie.editCategorie(cat);
-=======
+		adminCategorie.editCategorie(pcategorie);
+
 		//modif
-		iAdminCategoriesMetier.editCategorie(pcategorie);
->>>>>>> branch 'master' of https://github.com/MaximeBarbier/projet_groupe_ecommerce
-		
-<<<<<<< HEAD
+		adminCategorie.editCategorie(pcategorie);
+
 		model.addAttribute("categoriesAttribute", adminCategorie.getListCategories());
-=======
+
 
 		// recup les fonctionnaires de la bdd + renvoi des données vers
 		// fonctionnaires.jsp
-		model.addAttribute("categoriesAttribute", iAdminCategoriesMetier.getListCategories());
->>>>>>> branch 'master' of https://github.com/MaximeBarbier/projet_groupe_ecommerce
+		model.addAttribute("categoriesAttribute", adminCategorie.getListCategories());
+
 		
 
 		// redirection vers fonctionnaires.jsp
 		return "redirect:/listCategories";
 	}
-	
-	
 
-	
-	
-	
 	/**
 	 * Méthode pour afficher la photo
+	 * 
 	 * @return
 	 */
-	@RequestMapping(value="/photo", method=RequestMethod.GET)
-	public String getPhoto(@RequestParam(required=true, value="cat") Categorie cat, Model model) {
-		
+	@RequestMapping(value = "/photo", method = RequestMethod.GET)
+	public String getPhoto(@RequestParam(required = true, value = "cat") Categorie cat, Model model) {
+
 		String photoController = cat.getPhoto();
-		
-		
+
 		return photoController;
 	}
-	
-	
-	
+
 	/**
 	 * ajout d'une cat a la bdd
+	 * 
 	 * @return
 	 */
 	@RequestMapping(value = "/addCat", method = RequestMethod.POST)
-	public String addCategorie (@ModelAttribute("categorieCommand") 
-    							@Validated Categorie pcategorie) {
-		
-		//ajout d'une categorie
+	public String addCategorie(@ModelAttribute("categorieCommand") @Validated Categorie pcategorie) {
+
+		// ajout d'une categorie
 		adminCategorie.addCategorie(pcategorie);
 
-
 		return "redirect:/listCategories";
-		
+
 	}
-	
-	
+
 }
